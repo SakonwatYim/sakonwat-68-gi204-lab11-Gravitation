6cbf4ca20db42405bcbb5a79b34178df10ad19a0
SakonwatYim <sakonwat.yimnapoe@gmail.com>
2026-03-24 10:39:15 +0700

    forceMagnitude

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,16 +1,54 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    const float G = 0.006674f; //Gravitinal constant 6.674
+
+    public static List<Gravity> otherObjectList;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (otherObjectList == null)
+        {
+            otherObjectList = new List<Gravity>();
+        }
+
+        otherObjectList.Add(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        foreach (Gravity obj in otherObjectList)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
     }
+    void Attract (Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        //get direction between 2 objs
+        Vector3 direction = rb.position - otherRb.position;
+
+        //get only distance between 2 objs
+        float distance = direction.magnitude;
+        
+        //if 2 objs are at the same position, just return = do nothing to avoid collition
+        if(distance == 0f) {return; }
+
+        //F = G*((m1*m2)/r*r
+        float forceMagnitude = G * (rb.mass * otherRb.mass) / Mathf.Pow(distance, 2);
+
+        //combine force manitude with its direction (nomalize)
+        //to from final gravitinal force
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(gravityForce);
+    } 
 }
